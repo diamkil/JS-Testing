75574e08d6422331cd150a00b1932a1e677d31c4
diamkil <35732047+diamkil@users.noreply.github.com>
2020-08-14 17:37:05 -0400

    Added tests to divide

--- tests/math/divide.test.js
+++ tests/math/divide.test.js
@@ -4,6 +4,14 @@ test('dividing two numbers to give a positive number (w/o decimals)', () => {
 	expect(divide(10, 5)).toBe(2);
 });
 
+test('dividing two numbers to give a negative number (w/o decimals)', () => {
+	expect(divide(10, -2)).toBe(-5);
+});
+
 test('dividing two numbers to give a positive number (w/ decimals)', () => {
 	expect(divide(1787, 735)).toBe(2.43);
+});
+
+test('dividing two numbers to give a negative number (w/ decimals)', () => {
+	expect(divide(10, -7)).toBe(-1.43);
 });
